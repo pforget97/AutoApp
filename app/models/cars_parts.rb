d1eb6315f3518fa3ad852f3b3ed8880593ed0410
Patricia Forget <pforget@msudenver.edu>
2019-09-25 21:06:46 -0600

    Generated CarsParts model with attributes car:references, part:references

---
+++ app/models/cars_parts.rb
@@ -0,0 +1,4 @@
+class CarsParts < ApplicationRecord
+  belongs_to :car
+  belongs_to :part
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_212528) do
+ActiveRecord::Schema.define(version: 2019_09_26_030529) do
 
   create_table "cars", force: :cascade do |t|
     t.string "vin"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_09_25_212528) do
     t.datetime "updated_at", null: false
     t.index ["make_id"], name: "index_cars_on_make_id"
     t.index ["model_id"], name: "index_cars_on_model_id"
+  end
+
+  create_table "cars_parts", force: :cascade do |t|
+    t.integer "car_id"
+    t.integer "part_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_cars_parts_on_car_id"
+    t.index ["part_id"], name: "index_cars_parts_on_part_id"
   end
 
   create_table "makes", force: :cascade do |t|
