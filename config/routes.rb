bc2da633a5d31afa544e5ec85d2199984209408e
Patricia Forget <pforget@msudenver.edu>
2019-10-06 23:38:55 -0600

    Edit to routes file adding search feature to each resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
-  resources :parts
-  resources :cars
-  resources :models
-  resources :makes
+
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :makes do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :models do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :parts do
+    collection do
+      get 'search'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
