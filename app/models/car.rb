c8239373ec4b21bc587ff4adb0e57d53e679086c
Patricia Forget <pforget@msudenver.edu>
2019-10-06 23:15:25 -0600

    Added vaidations to Car class, requires 8 digit vin, make and model.  Added validation to Make, requires name

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,7 @@ class Car < ApplicationRecord
   belongs_to :make
   belongs_to :model
   has_and_belongs_to_many :parts
+
+  validates :make, :model, presence: true
+  validates :vin, length: {is: 8}
 end
--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,5 @@
 class Make < ApplicationRecord
   has_many :models
+
+  validates :name, presence: true
 end
